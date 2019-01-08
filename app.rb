5f97b5769d7aa1b8618c9bd9ffd1432242de89a9
Vitaliy <ibukreev@users.noreply.github.com>
2019-01-09 01:43:33 +0200

    Update ui

--- app.rb
+++ app.rb
@@ -50,7 +50,7 @@ configure do
 end
 
 get '/' do
-	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School!!!</a>"			
+	erb "<h2>Добро пожаловать в Barber Shop</h2>"			
 end
 
 get '/about' do
